ddde520224b7fc5c0b0c4f4fd0b6efbf78093e1f
Bryan Aguinaldo <bryanaguinaldo.e@gmail.com>
2023-07-06 20:55:39 +0800

    added GA

--- src/app/components/GoogleAnalytics.js
+++ src/app/components/GoogleAnalytics.js
@@ -4,14 +4,14 @@ import Script from "next/script";
 function GoogleAnalytics() {
     return (
         <>
-            <Script src="https://www.googletagmanager.com/gtag/js?id=G-J9R731N32B" />
+            <Script src="https://www.googletagmanager.com/gtag/js?id=G-6EEG5Z9ZP2" />
             <Script id="google-analytics">
                 {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', 'G-J9R731N32B');
+          gtag('config', 'G-6EEG5Z9ZP2');
         `}
             </Script>
         </>
